0fae860f47b5d9ecaeaf479ce763d9b6c87cb97e
aeklundh <eklundh.albin@gmail.com>
2017-08-23 17:46:01 +0200

    add dish menu status

--- PizzeriaDelish/Models/Ingredient.cs
+++ PizzeriaDelish/Models/Ingredient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace PizzeriaDelish.Models
     public class Ingredient
     {
         public int IngredientId { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public ICollection<DishIngredient> DishIngredients { get; set; }
