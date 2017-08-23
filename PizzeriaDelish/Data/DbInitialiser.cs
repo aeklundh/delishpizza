42f9c13827905dd5ace46e67609756072ccb81ab
aeklundh <eklundh.albin@gmail.com>
2017-08-23 14:12:42 +0200

    add basic seeding

--- PizzeriaDelish/Data/DbInitialiser.cs
+++ PizzeriaDelish/Data/DbInitialiser.cs
@@ -9,9 +9,76 @@ namespace PizzeriaDelish.Data
 {
     public static class DbInitialiser
     {
-        public static void Initialise(WebshopDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public static async void Initialise(WebshopDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            
+            IdentityRole adminRole = new IdentityRole() { Name = "Admin" };
+            await roleManager.CreateAsync(adminRole);
+
+            ApplicationUser seededUser = new ApplicationUser()
+            {
+                UserName = "ture@test.net",
+                Email = "ture@test.net",
+                Address = "Hemvägen 5",
+                City = "Huddinge",
+                PostalCode = "14140",
+                FirstName = "Ture",
+                Surname = "Testare"
+            };
+            ApplicationUser adminUser = new ApplicationUser()
+            {
+                UserName = "admin@delish.se",
+                Email = "admin@delish.se",
+                Address = "Bortgången 3",
+                City = "Molnet",
+                PostalCode = "14141",
+                FirstName = "Admin",
+                Surname = "Admin"
+            };
+            await userManager.CreateAsync(seededUser, "Test12#");
+            await userManager.CreateAsync(adminUser, "PassWeCan2!");
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+
+            List<Category> categories = new List<Category>() {
+                new Category() { Name = "Pizza" },
+                new Category() { Name = "Sallad" },
+                new Category() { Name = "Tillbehör" },
+                new Category() { Name = "Kebab" },
+                new Category() { Name = "Pasta" },
+                new Category() { Name = "Övrigt" }
+            };
+            List<Ingredient> ingredients = new List<Ingredient>() {
+                new Ingredient() { Name = "Skinka" },
+                new Ingredient() { Name = "Ananas" },
+                new Ingredient() { Name = "Champinjoner" }
+            };
+            List<Dish> dishes = new List<Dish>() {
+                new Dish() { Name = "Margherita",  },
+                new Dish() { Name = "Vesuvio" },
+                new Dish() { Name = "Hawaii" },
+                new Dish() { Name = "Capricciosa" }
+            };
+            List<DishCategory> dishCategories = new List<DishCategory>() {
+                new DishCategory() { Dish = dishes[0], Category = categories[0] },
+                new DishCategory() { Dish = dishes[1], Category = categories[0] },
+                new DishCategory() { Dish = dishes[2], Category = categories[0] },
+                new DishCategory() { Dish = dishes[3], Category = categories[0] }
+            };
+            List<DishIngredient> dishIngredients = new List<DishIngredient>() {
+                new DishIngredient() { Dish = dishes[1], Ingredient = ingredients[0] },
+                new DishIngredient() { Dish = dishes[2], Ingredient = ingredients[0] },
+                new DishIngredient() { Dish = dishes[2], Ingredient = ingredients[1] },
+                new DishIngredient() { Dish = dishes[3], Ingredient = ingredients[0] },
+                new DishIngredient() { Dish = dishes[3], Ingredient = ingredients[2] }
+            };
+
+            context.Categories.AddRange(categories);
+            context.Ingredients.AddRange(ingredients);
+            context.Dishes.AddRange(dishes);
+            context.DishCategories.AddRange(dishCategories);
+            context.DishIngredients.AddRange(dishIngredients);
+
+            context.SaveChanges();
+
         }
     }
 }
