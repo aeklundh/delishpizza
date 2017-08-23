64e227b3a99033e99a1bfd4110bd2860d16b3d19
aeklundh <eklundh.albin@gmail.com>
2017-08-23 14:12:19 +0200

    add postalcode

--- PizzeriaDelish/Models/ApplicationUser.cs
+++ PizzeriaDelish/Models/ApplicationUser.cs
@@ -6,12 +6,13 @@ using Microsoft.AspNetCore.Identity;
 
 namespace PizzeriaDelish.Models
 {
-    // Add profile data for application users by adding properties to the WebshopUser class
+    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
+        public string PostalCode { get; set; }
     }
 }
