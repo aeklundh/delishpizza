9669bc2ca630c1fd6a309a83cdb58a5ab3897a10
aeklundh <eklundh.albin@gmail.com>
2017-08-24 16:08:04 +0200

    add cartitem model

---
+++ PizzeriaDelish/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzeriaDelish.Models
+{
+    public class CartItem
+    {
+        public Dish Dish { get; set; }
+        public List<CustomIngredient> CustomIngredients { get; set; }
+    }
+}
