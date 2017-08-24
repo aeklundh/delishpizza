983295d44080012ca98da9808a2635fc27817f02
aeklundh <eklundh.albin@gmail.com>
2017-08-24 16:06:45 +0200

    add cart extensions

---
+++ PizzeriaDelish/Extensions/CartExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using PizzeriaDelish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzeriaDelish.Extensions
+{
+    public static class CartExtensions
+    {
+        public static bool CartIsEmpty(this ISession session)
+        {
+            return String.IsNullOrWhiteSpace(session.GetString("cart"));
+        }
+
+        public static void SerialiseCart(this ISession session, ICollection<CartItem> cart)
+        {
+            session.SetString("cart", JsonConvert.SerializeObject(cart));
+        }
+
+        public static List<CartItem> DeserialiseCart(this ISession session)
+        {
+            return JsonConvert.DeserializeObject<List<CartItem>>(session.GetString("cart"));
+        }
+    }
+}
