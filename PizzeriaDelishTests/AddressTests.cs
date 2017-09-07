87cd353e426e8c61e086cab5ead934154b4308c6
aeklundh <eklundh.albin@gmail.com>
2017-09-07 15:21:32 +0200

    add test project, a base test and address test

---
+++ PizzeriaDelishTests/AddressTests.cs
@@ -0,0 +1,45 @@
+﻿using PizzeriaDelish.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PizzeriaDelish.Models;
+using Xunit;
+using PizzeriaDelish.Services;
+
+namespace PizzeriaDelishTests
+{
+    public class AddressTests : BaseTest
+    {
+        private WebshopDbContext _context { get; set; }
+
+        protected override void InitialiseDatabase()
+        {
+            _context = _serviceProvider.GetService<WebshopDbContext>();
+
+            List<Address> addresses = new List<Address>()
+            {
+                new Address() { City = "Stockholm", StreetAddress = "Storstorevägen 3", PostalCode="12345", FirstName = "Egon", Surname = "Söderblombom" },
+                new Address() { City = "Turku", StreetAddress = "Åbovägen 55A", PostalCode="54321", FirstName = "Ålmar", Surname = "Ulman" }
+            };
+            _context.Addresses.AddRange(addresses);
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public async void Add_Address()
+        {
+            //Assemble
+            AddressService addressService = _serviceProvider.GetService<AddressService>();
+            Address expected = new Address() { City = "AAA", StreetAddress = "Åbovägen 55A", PostalCode = "54321", FirstName = "Ålmar", Surname = "Ulman" };
+            
+            //Act
+            Address actual1 = await addressService.AddAddressAsync(expected);
+            Address actual2 = await addressService.AddAddressAsync(expected);
+
+            //Assert
+            Assert.True(actual1.AddressId == 3);
+            Assert.True(actual1.AddressId == actual2.AddressId);
+        }
+    }
+}
---
+++ PizzeriaDelishTests/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PizzeriaDelish.Data;
+using PizzeriaDelish.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzeriaDelishTests
+{
+    public abstract class BaseTest
+    {
+        protected readonly IServiceProvider _serviceProvider;
+
+        protected BaseTest()
+        {
+            ServiceProvider efServiceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<WebshopDbContext>(b =>
+                b.UseInMemoryDatabase("dejtabaes")
+                .UseInternalServiceProvider(efServiceProvider));
+
+            services.AddTransient<AddressService>();
+            services.AddTransient<AdminService>();
+            services.AddTransient<CartService>();
+            services.AddTransient<CheckoutService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            InitialiseDatabase();
+        }
+
+        protected virtual void InitialiseDatabase()
+        { }
+    }
+}
